6f6e6fb036f6aa716828875ddb156c0ffbdf4639
heisdanny64 <1.95826997e+08+heisdanny64@users.noreply.github.com>
2026-08-17 21:05:40 +0000

    Store MovieBox subject IDs as exact strings

--- migrations/004_moviebox_ids.sql
+++ migrations/004_moviebox_ids.sql
@@ -1,4 +1,4 @@
-ALTER TABLE media_titles ADD COLUMN IF NOT EXISTS moviebox_id BIGINT;
+ALTER TABLE media_titles ADD COLUMN IF NOT EXISTS moviebox_id TEXT;
 
 CREATE UNIQUE INDEX IF NOT EXISTS media_titles_moviebox_id_unique_idx
   ON media_titles (moviebox_id)
